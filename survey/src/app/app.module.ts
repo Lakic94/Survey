3eb2ab1df0961781e490f20d00885b13d0a919c8
Nikola <nilakic94@gmail.com>
2019-08-03 23:05:37 +0200

    Rutiranje dodato

--- survey/src/app/app.module.ts
+++ survey/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateSurveyComponent } from './create-survey/create-survey.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateSurveyComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
